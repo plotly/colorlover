be23d35bbd6627e563906069ec3769b28a937929
Jack <jack@Jacks-MacBook-Air.local>
2015-01-29 23:13:49 -0500

    luv the colors

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'colorlover',
+  packages = ['colorlover'], # this must be the same as the name above
+  version = '0.2',
+  description = 'Color scales for IPython notebook',
+  author = 'Jack Parmer',
+  author_email = 'jack@plot.ly',
+  url = 'https://github.com/jackparmer/colorlover', # use the URL to the github repo
+  keywords = ['ipython notebook','color scales'], # arbitrary keywords
+  classifiers = [],
+)
