45af8e89d9ea0d268cbf67b4ad0349e9b87e1308
Jon Mease <jon.mease@gmail.com>
2019-01-19 10:39:28 -0500

    Hsl fixes (#25)
    Fixes #18 by interpolating the Hue component of HSL colors as angles (rather that a pure linear interpolation). For example, the angle half way between 10 deg and 350 deg should be 0 not 180.
    
    Also fixed an error in the calculation of the Hue component in rgb_to_hsl

--- colorlover/__init__.py
+++ colorlover/__init__.py
@@ -1778,67 +1778,88 @@ def flipper( scl=None ):
             flipped[subkey][key] = subval
     return flipped
 
+
+def rgb_to_hsl(rgb):
+    ''' Adapted from M Bostock's RGB to HSL converter in d3.js
+        https://github.com/mbostock/d3/blob/master/src/color/rgb.js '''
+    r,g,b = float(rgb[0])/255.0,\
+            float(rgb[1])/255.0,\
+            float(rgb[2])/255.0
+    mx = max(r, g, b)
+    mn = min(r, g, b)
+    h = s = l = (mx + mn) / 2
+    if mx == mn: # achromatic
+        h = 0
+        s = 0 if l > 0 and l < 1 else h
+    else:
+        d = mx - mn;
+        s =  d / (mx + mn) if l < 0.5 else d / (2 - mx - mn)
+        if mx == r:
+            h = (g - b) / d + ( 6 if g < b else 0 )
+        elif mx == g:
+            h = (b - r) / d + 2
+        else:
+            h = (r - g) / d + 4
+
+    return (int(round(h*60,4)), int(round(s*100,4)), int(round(l*100,4)))
+
+
 def interp(scl, r):
     ''' Interpolate a color scale "scl" to a new one with length "r"
         Fun usage in IPython notebook:
         HTML( to_html( to_hsl( interp( cl.scales['11']['qual']['Paired'], 5000 ) ) ) ) '''
     c = []
-    SCL_FI = len(scl)-1 # final index of color scale
-    # garyfeng:
-    # the following line is buggy.
-    # r = [x * 0.1 for x in range(r)] if isinstance( r, int ) else r
-    r = [x*1.0*SCL_FI/r for x in range(r)] if isinstance( r, int ) else r
-    # end garyfeng
 
-    scl = to_numeric( scl )
-
-    def interp3(fraction, start, end):
-        ''' Interpolate between values of 2, 3-member tuples '''
-        def intp(f, s, e):
-            return s + (e - s)*f
-        return tuple([intp(fraction, start[i], end[i]) for i in range(3)])
-
-    def rgb_to_hsl(rgb):
-        ''' Adapted from M Bostock's RGB to HSL converter in d3.js
-            https://github.com/mbostock/d3/blob/master/src/color/rgb.js '''
-        r,g,b = float(rgb[0])/255.0,\
-                float(rgb[1])/255.0,\
-                float(rgb[2])/255.0
-        mx = max(r, g, b)
-        mn = min(r, g, b)
-        h = s = l = (mx + mn) / 2
-        if mx == mn: # achromatic
-            h = 0
-            s = 0 if l > 0 and l < 1 else h
+    if isinstance(r, int):
+        if r == 0:
+            r_steps = []
+        elif r == 1:
+            # Midway point
+            r_steps = [(len(scl) / 2) - 1]
         else:
-            d = mx - mn;
-            s =  d / (mx + mn) if l < 0.5 else d / (2 - mx - mn)
-            if mx == r:
-                h = (g - b) / d + ( 6 if g < b else 0 )
-            elif mx == g:
-                h = (b - r) / d + 2
+            # Linearly space r from 0 to len(scl) - 1
+            r_steps = [x*1.0*(len(scl) - 1)/(r-1) for x in range(r)]
+    else:
+        r_steps = r
+
+    scl = to_numeric(scl)
+
+    def hsl_interp(fraction, hsl_start, hsl_end):
+        ''' Interpolate between values of 2, 3-member tuples '''
+
+        def interp_linear(f, s, e):
+            return s + (e - s) * f
+
+        def interp_circular(f, s, e):
+            s_mod = s % 360
+            e_mod = e % 360
+            if max(s_mod, e_mod) - min(s_mod, e_mod) > 180:
+                s_mod, e_mod = (s_mod + 360, e_mod) if s_mod < e_mod else (
+                s_mod, e_mod + 360)
+                return interp_linear(f, s_mod, e_mod) % 360
             else:
-                h = r - g / d + 4
+                return interp_linear(f, s_mod, e_mod)
+        return (
+            interp_circular(fraction, hsl_start[0], hsl_end[0]),
+            interp_circular(fraction, hsl_start[1], hsl_end[1]),
+            interp_circular(fraction, hsl_start[2], hsl_end[2]),
+        )
 
-        return (int(round(h*60,4)), int(round(s*100,4)), int(round(l*100,4)))
-
-    for i in r:
-        # garyfeng: c_i could be rounded up so scl[c_i+1] will go off range
-        #c_i = int(i*math.floor(SCL_FI)/round(r[-1])) # start color index
-        #c_i = int(math.floor(i*math.floor(SCL_FI)/round(r[-1]))) # start color index
-        #c_i = if c_i < len(scl)-1 else hsl_o
-
-        c_i = int(math.floor(i))
+    for i in r_steps:
         section_min = math.floor(i)
         section_max = math.ceil(i)
-        fraction = (i-section_min) #/(section_max-section_min)
 
-        hsl_o = rgb_to_hsl( scl[c_i] ) # convert rgb to hls
-        hsl_f = rgb_to_hsl( scl[c_i+1] )
-        #section_min = c_i*r[-1]/SCL_FI
-        #section_max = (c_i+1)*(r[-1]/SCL_FI)
-        #fraction = (i-section_min)/(section_max-section_min)
-        hsl = interp3( fraction, hsl_o, hsl_f )
+        c_i_min = int(section_min)
+        c_i_max = int(section_max)
+
+        if c_i_min == c_i_max:
+            # No interpolation needed
+            hsl = rgb_to_hsl(scl[c_i_min])
+        else:
+            fraction = (i-section_min)
+            hsl_o = rgb_to_hsl( scl[c_i_min] ) # convert rgb to hls
+            hsl_f = rgb_to_hsl( scl[c_i_max] )
+            hsl = hsl_interp( fraction, hsl_o, hsl_f )
         c.append( 'hsl'+str(hsl) )
 
     return to_hsl( c )
--- tests.py
+++ tests.py
@@ -3,6 +3,27 @@ import unittest
 import colorlover as cl
 
 
+rgb_hsl_colors = {
+            (0, 0, 0): 	(0, 0, 0),  # Black
+            (255, 255, 255): 	(0, 0, 100),  # White
+            (255, 0, 0): 	(0, 100, 50),  # Red
+            (0, 255, 0): 	(120, 100, 50),  # Lime
+            (0, 0, 255): 	(240, 100, 50),  # Blue
+            (255, 255, 0): 	(60, 100, 50),  # Yellow
+            (0, 255, 255): 	(180, 100, 50),  # Cyan
+            (255, 0, 255): 	(300, 100, 50),  # Magenta
+            (192, 192, 192): 	(0, 0, 75),  # Silver
+            (128, 128, 128): 	(0, 0, 50),  # Gray
+            (128, 0, 0): 	(0, 100, 25),  # Maroon
+            (128, 128, 0): 	(60, 100, 25),  # Olive
+            (0, 128, 0): 	(120, 100, 25),  # Green
+            (128, 0, 128): 	(300, 100, 25),  # Purple
+            (0, 128, 128): 	(180, 100, 25),  # Teal
+            (0, 0, 128): 	(240, 100, 25),  # Navy
+            (20, 30, 128): 	(234, 72, 29)  # Near navy
+}
+
+
 class UsageTests(unittest.TestCase):
     def test_scales(self):
         scales = cl.scales['3']['div']['RdYlBu']
@@ -51,6 +72,13 @@ class UsageTests(unittest.TestCase):
             flipped,
             ['rgb(252,141,89)', 'rgb(255,255,191)', 'rgb(145,191,219)']
         )
+
+    def test_rgb_to_hsl(self):
+        # Make sure interpolating a colorscale to the same length does nothing
+        # but change colorspace from hsl to rgb
+        for rgb, expected_hsl in rgb_hsl_colors.items():
+            hsl = cl.rgb_to_hsl(rgb)
+            self.assertEqual(hsl, expected_hsl)
 
 
 if __name__ == '__main__':
